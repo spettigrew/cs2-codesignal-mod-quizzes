a5e8514e6b286433899fd729f274e8f583e898a7
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-12-02 21:09:00 -0700

    Swapcase with slicing involved

---
+++ python_basics/opposite_reverse.py
@@ -0,0 +1,20 @@
+"""
+Write a function that takes a string as input and returns that string in reverse order, with the opposite casing for each character within the string.
+
+Examples:
+
+csOppositeReverse("Hello World") ➞ "DLROw OLLEh"
+csOppositeReverse("ReVeRsE") ➞ "eSrEvEr"
+csOppositeReverse("Radar") ➞ "RADAr"
+Notes:
+
+The input string will only contain alpha characters.
+[execution time limit] 4 seconds (py3)
+
+[input] string txt
+
+[output] string
+"""
+
+def csOppositeReverse(txt):
+    return txt[::-1].swapcase()
