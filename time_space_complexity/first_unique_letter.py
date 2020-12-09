11e5d0ae31daa3766abf986080c00f643c012ef4
Sara Pettigrew <sarajp.ak@hotmail.com>
2020-12-09 16:19:08 -0700

    Code and notes included

--- time_space_complexity/first_unique_letter.py
+++ time_space_complexity/first_unique_letter.py
@@ -18,15 +18,29 @@ input_str will only contain lowercase alpha characters.
 """
 
 def csFirstUniqueChar(input_str):
-    # U - return the index of the first non-repeating char. If none, return -1
-    # set unique character to 0
-    uni_char = 0
-    
+    #     U - return the index of the first non-repeating char. If none, return -1   
     # iterate through the string to find first unique char
-    # for char in input_str:
+        
+    for i in range(len(input_str)):
+        if input_str.count(input_str[i]) == 1:
+            return i
+    
+    return -1
+        
+
+
+
+
+
+    # # U - return the index of the first non-repeating char. If none, return -1
+    # # set unique character to 0
+    # uni_char = 0
+    
+    # # iterate through the string to find first unique char
+    # # for char in input_str:
         
     
-    # If none, return -1
-    if uni_char == 0:
-        return -1
+    # # If none, return -1
+    # if uni_char == 0:
+    #     return -1   # only 2/5 tests passing
 
