dfeff8b906849280c2885730a408d38db031b837
Sara Pettigrew <sarajp.ak@hotmail.com>
2020-12-15 22:21:01 -0700

    Code and solution added

---
+++ time_space_complexity/first_unique_char.py
@@ -0,0 +1,28 @@
+"""
+Given a string, write a function that returns the index of the first unique (non-repeating) character. If there isn't a unique (non-repeating) character, return -1.
+
+Examples:
+
+csFirstUniqueChar(input_str = "lambdaschool") -> 2
+csFirstUniqueChar(input_str = "ilovelambdaschool") -> 0
+csFirstUniqueChar(input_str = "vvv") -> -1
+Notes:
+
+input_str will only contain lowercase alpha characters.
+[execution time limit] 4 seconds (py3)
+
+[input] string input_str
+
+[output] integer
+"""
+
+def csFirstUniqueChar(input_str):
+    #     U - return the index of the first non-repeating char. If none, return -1   
+    # iterate through the string to find first unique char
+        
+    for i in range(len(input_str)):
+        if input_str.count(input_str[i]) == 1:
+            return i
+    
+    return -1
+        
