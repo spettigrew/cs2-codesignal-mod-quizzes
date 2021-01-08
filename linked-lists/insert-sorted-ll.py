214a93cd45335e45e04e8294533b1d29050defb9
Sara Pettigrew <sarajp.ak@hotmail.com>
2021-01-07 20:13:15 -0700

    Alternate code and notes added

--- linked-lists/insert-sorted-ll.py
+++ linked-lists/insert-sorted-ll.py
@@ -72,34 +72,34 @@ def insertValueIntoSortedLinkedList(l, value):
 
 # -------------- Alternative code -----------------------
 
-    # create a new node with the value
-    node = ListNode(value)
-    # if there is no list return the new node
-    if l == None:
-        return node
-    else:
-        # else if the list.value (first item in the list) > the new value
-        if l.value > value:
-            # set new values as the first item in the list
-            node.next = l
-            return node
-        else:
-            # else create a temp value for the current list item and the previous set to None
-            temp, prev = l, None
-            # while the there is a next item and current item value is less
-            # than the value iterate
-            while temp.next and temp.value <= value:
-                # set previous to temp and temp to next
-                prev = temp
-                temp = temp.next
-            # check if temp.next is None (last item) and still not larger than the value
-            if temp.next == None and temp.value <= value:
-                # if so add the value as the last item in the list since it is the largest
-                temp.next = node
-            else:
-                # else if the next item is larger than the value set the next item as the next item of the new value
-                node.next = prev.next
-                # and set the previous item to point to the new value next
-                prev.next = node
-            # return the list
-            return l
+    # # create a new node with the value
+    # node = ListNode(value)
+    # # if there is no list return the new node
+    # if l == None:
+    #     return node
+    # else:
+    #     # else if the list.value (first item in the list) > the new value
+    #     if l.value > value:
+    #         # set new values as the first item in the list
+    #         node.next = l
+    #         return node
+    #     else:
+    #         # else create a temp value for the current list item and the previous set to None
+    #         temp, prev = l, None
+    #         # while the there is a next item and current item value is less
+    #         # than the value iterate
+    #         while temp.next and temp.value <= value:
+    #             # set previous to temp and temp to next
+    #             prev = temp
+    #             temp = temp.next
+    #         # check if temp.next is None (last item) and still not larger than the value
+    #         if temp.next == None and temp.value <= value:
+    #             # if so add the value as the last item in the list since it is the largest
+    #             temp.next = node
+    #         else:
+    #             # else if the next item is larger than the value set the next item as the next item of the new value
+    #             node.next = prev.next
+    #             # and set the previous item to point to the new value next
+    #             prev.next = node
+    #         # return the list
+    #         return l
